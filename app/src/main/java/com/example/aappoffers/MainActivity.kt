ed38c38163156eae8090d204407415833d88fcd5
WoobeeAU <brent.mcdowall@gmail.com>
2019-11-16 09:23:04 +1000

    Try radar.io

--- app/src/main/java/com/example/aappoffers/MainActivity.kt
+++ app/src/main/java/com/example/aappoffers/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.aappoffers
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import com.example.aappoffers.MainActivity.ActConstants.MY_PERMISSIONS_REQUEST_LOCATION
+import io.radar.sdk.Radar
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    object ActConstants {
+        const val MY_PERMISSIONS_REQUEST_LOCATION = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        // background and targeting API level 29 and higher
+        Radar.initialize("prj_test_pk_e95b6e7f697c3d8cf9dc73b92382063876792007")
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION), MY_PERMISSIONS_REQUEST_LOCATION)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
@@ -36,4 +50,33 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                val textView = findViewById<TextView>(R.id.text_view_id2)
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Radar.startTracking()
+                    textView.setText("Location Permission granted").toString()
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Radar.stopTracking()
+                    textView.setText("Location Permission denied").toString()
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
 }
